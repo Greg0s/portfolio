7c1a48143c6d7ebd677b78c711d1e94444e98a83
Greg0s <gregoire.tinnes@outlook.fr>
2023-08-07 00:07:18 +0200

    update lang: about, web dev

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,46 +1,61 @@
+let clang = getCookie("currentlang");
+let currentLang = "fr";
+if (clang) {
+  currentLang = clang;
+}
+loadLang();
 
-    let clang=getCookie("currentlang")
-    let currentLang="fr"
-    if(clang){
-      currentLang=clang
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-    loadLang()
-
-
-    function setCookie(cname, cvalue, exdays){
-      var d = new Date();
-      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-      var expires = "expires="+d.toUTCString();
-      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
     }
+  }
+  return "";
+}
 
-    function getCookie(cname) {
-      var name = cname + "=";
-      var decodedCookie = decodeURIComponent(document.cookie);
-      var ca = decodedCookie.split(';');
-      for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
-      }
-      return "";
-    }
+function langFR() {
+  rmActive("en");
+  currentLang = "fr";
+  loadLang();
+}
 
-    function langFR(){
-      currentLang = "fr";
-      loadLang()
-    }
+function langEN() {
+  rmActive("fr");
+  currentLang = "en";
+  loadLang();
+}
 
-    function langEN(){
-      currentLang = "en";
-      loadLang()
-    }
+function setActive(lang) {
+  let className = "." + lang;
+  if (document.querySelector(className)) {
+    console.log(className);
+    document.querySelector(className).classList.add("active");
+  }
+}
 
-    function loadLang(){
-      setCookie("currentlang",currentLang,30);
-      $('[data-localize]').localize("assets/lang/lang", {language: currentLang});
-    }
+function rmActive(lang) {
+  let className = "." + lang;
+  console.log(className);
+  document.querySelector(className).classList.remove("active");
+}
+
+function loadLang() {
+  setCookie("currentlang", currentLang, 30);
+  $("[data-localize]").localize("assets/lang/lang", { language: currentLang });
+  setActive(currentLang);
+}
